b3e1cf7d2ef96c986713e12d4e45f1b5cc76f9ac
Desmarchelier Maxime <maxime.desmarchelier@ensiie.fr>
2023-08-05 22:01:14 -0400

    Fix getTrack(ID)

--- database.js
+++ database.js
@@ -1,33 +1,35 @@
 import PocketBase from "pocketbase/cjs";
 
 export default class Database {
-  constructor() {
-    this.pb = new PocketBase('http://127.0.0.1:8090');
-    this.username = process.env.PB_USERNAME;
-    this.password = process.env.PB_PASSWORD;
-  }
-
-  async _checkAuthentication() {
-    if (!this.pb.authStore.isValid) {
-      await this.pb.admins.authWithPassword(this.username, this.password);
+    constructor() {
+        this.pb = new PocketBase('http://127.0.0.1:8090');
+        this.username = process.env.PB_USERNAME;
+        this.password = process.env.PB_PASSWORD;
     }
-  }
 
-  async getTrackList() {
-    await this._checkAuthentication();
-    return await this.pb.collection('Track').getFullList({
-      sort: '-created',
-    });
-  }
+    async _checkAuthentication() {
+        if (!this.pb.authStore.isValid) {
+            await this.pb.admins.authWithPassword(this.username, this.password);
+        }
+    }
 
-  async getTrack(trackId) {
-    await this._checkAuthentication();
-    return await this.pb.collection('Track').get(trackId);
-  }
+    async getTrackList() {
+        await this._checkAuthentication();
+        return await this.pb.collection('Track').getFullList({
+            sort: '-created',
+        });
+    }
 
-  async addTrack(track) {
-    try {
-      await this._checkAuthentication();
+    async getTrack(trackId) {
+        await this._checkAuthentication();
+        return await this.pb.collection('Track').getFullList({
+            sort: '-created', filter: `id_track="${trackId}"`,
+        });
+    }
+
+    async addTrack(track) {
+        try {
+            await this._checkAuthentication();
 
             const data = {
                 "id_track": track.id,
@@ -60,21 +62,19 @@ export default class Database {
             };
 
             return await this.pb.collection('User').create(data);
-        }
-        catch (error) {
+        } catch (error) {
             if (error.status !== 400) {
                 console.error('An error occurred while adding the user:', error);
             }
         }
     }
 
-  async getUser(userId) {
-    await this._checkAuthentication();
-    return await this.pb.collection('User').getFullList({
-      sort: '-created',
-      filter: 'id_user="'+ userId +'"',
-    });
-  }
+    async getUser(userId) {
+        await this._checkAuthentication();
+        return await this.pb.collection('User').getFullList({
+            sort: '-created', filter: 'id_user="' + userId + '"',
+        });
+    }
 
     async getUsersList() {
         await this._checkAuthentication();
@@ -128,8 +128,7 @@ export default class Database {
         //     sort: '-created',
         // });
         return await this.pb.collection('Vote').getFullList({
-            sort: '-created',
-            filter: `created >= "${beginTime}" && created <= "${stopTime}" && user_id="${userId}"`,
+            sort: '-created', filter: `created >= "${beginTime}" && created <= "${stopTime}" && user_id="${userId}"`,
         });
 
     }
