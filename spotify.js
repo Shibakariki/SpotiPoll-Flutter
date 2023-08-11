3e928998ea09b3dc5e8ff753cfe361e891bba2d0
Maxime Desmarchelier <maxime.desmarchelier@gmail.com>
2023-08-11 10:20:28 -0400

    Fix added_by

--- spotify.js
+++ spotify.js
@@ -61,31 +61,74 @@ class SpotifyClient {
         return all_playlists;
     }
 
+    // TODO : Stocker en BDD, la correspondance nom / id spotify
     async getPlaylistTracks(playlist_id) {
         const response = await axios.get(`https://api.spotify.com/v1/playlists/${playlist_id}/tracks`, {
             headers: {
                 Authorization: "Bearer " + this.accessToken,
             },
         });
-
+    
         if (response.data.error) {
-            console.error("Une erreur s'est produite lors de la récupération des pistes de la playlist:",);
+            console.error("Une erreur s'est produite lors de la récupération des pistes de la playlist:");
             throw response.data.error;
         }
-
+    
+        // Step 1: Identifier tous les ID d'utilisateurs uniques qui ont ajouté des chansons.
+        const uniqueUserIds = new Set(response.data.items.map(item => item.added_by.id));
+    
+        // Step 2: Récupérer les noms de ces utilisateurs et les stocker dans un cache.
+        const userNameCache = {};
+        for (let userId of uniqueUserIds) {
+            const userProfile = await this.getUserProfile(userId);
+            userNameCache[userId] = userProfile.displayName || "Unknown";
+        }
+    
+        // Step 3: Mapper les informations de piste et utiliser le cache pour le nom de l'utilisateur.
         return response.data.items.map(item => {
             const track = item.track;
-            const added_by_id = item.added_by.id;
-
             return {
                 "id_track": track.id,
                 "name": track.name,
                 "artist": track.artists[0].name,
-                // TODO : Regarder dans la BDD le nom de l'utilisateur, sinon demander à Spotify
-                "adder": "NOT IMPLEMENTED ... YET",
+                "adder": userNameCache[item.added_by.id],
                 "url": track.external_urls.spotify,
             };
         });
+    }
+    
+
+    async getUserProfile(userId) {
+    
+        try {
+            const response = await axios.get(`https://api.spotify.com/v1/users/${userId}`, {
+                headers: {
+                    Authorization: "Bearer " + this.accessToken,
+                },
+            });
+    
+            if (response.data.error) {
+                console.error("Error fetching user profile:", response.data.error);
+                throw new Error(response.data.error);
+            }
+    
+            return {
+                displayName: response.data.display_name,
+                externalUrls: response.data.external_urls,
+                followers: {
+                    total: response.data.followers.total,
+                },
+                href: response.data.href,
+                id: response.data.id,
+                images: response.data.images,
+                type: response.data.type,
+                uri: response.data.uri
+            };
+    
+        } catch (error) {
+            console.error("Failed to fetch user profile:", error);
+            throw error;
+        }
     }
 
     async getPlaylistId() {
