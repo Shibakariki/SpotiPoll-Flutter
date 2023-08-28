277ac6c2867d237376f5f35aa0de581268b9b567
Shibakariki <73525386+Shibakariki@users.noreply.github.com>
2023-08-28 21:58:18 +0200

    Test avec pocketbase

--- launch.sh
+++ launch.sh
@@ -6,3 +6,7 @@ apt-get npm install -y
 npm install
 npm start
 
+curl https://github.com/pocketbase/pocketbase/releases/download/v0.17.7/pocketbase_0.17.7_linux_amd64.zip -o pocketbase.zip
+unzip pocketbase.zip
+chmod +x pocketbase
+./pocketbase serve --https="https://spotipoll.onrender.com:7035"
