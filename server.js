5d5ae07fe796fb5a131a5ed7706b2da9e1d22b7d
axelMennessier <axel.mennessier@vinci-energies.net>
2023-10-19 12:43:43 +0200

    c'est . et pas [""]

--- server.js
+++ server.js
@@ -365,7 +365,7 @@ app.get("/result", verifyToken, async (req, res) => {
         let vote = resultList[0]
         let track = await database.getTrack(vote["id_track"])
         console.log(track)
-        console.log(track[0])
+        console.log(track[0].name)
         if (track === undefined){
             let totalVotes = vote["yes_vote"] - vote["no_vote"]
             let result_vote = "";
